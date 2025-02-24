55984966d8ac3ed0ab28a4ba000eb0b3e385d2d3
MicahMaphet <micahmaphet@gmail.com>
2025-02-24 10:22:28 -0800

    readablity and congruence with repository

--- src/main/java/frc/robot/subsystems/coralmech/CoralMech.java
+++ src/main/java/frc/robot/subsystems/coralmech/CoralMech.java
@@ -11,37 +11,50 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Starting up and tuning procedure:
+ * 1. Find the right motor ids using ![REV Hardware Client](https://github.com/REVrobotics/REV-Software-Binaries/releases/download/rhc-1.7.3/REV-Hardware-Client-Setup-1.7.3.exe)
+ * 2. Tune the intake and eject voltages
+ * 3. Make sure inversions are done correctly, currently done in the methods rather than configuration
+ */
 public class CoralMech extends SubsystemBase {
     // We use REV-41-1600
     // Motor on the left of the coral mechanism, and the right of the coral mechanism
-    private static final SparkMax motorL = new SparkMax(0, MotorType.kBrushed);
-    private static final SparkMax motorR = new SparkMax(1, MotorType.kBrushed);
+    private static final SparkMax m_motorL = new SparkMax(0, MotorType.kBrushed); // TODO, find right id
+    private static final SparkMax m_motorR = new SparkMax(1, MotorType.kBrushed); // TODO, find right id
     // not currently in use
-    private static final DigitalInput sensor = new DigitalInput(0);
+    // Will be used to detect when a coral is in the mechanism, needed for knowing when to brake the motors
+    private static final DigitalInput m_sensor = new DigitalInput(0);
 
-    private static final double ejectVoltage = 1;
-    private static final double intakeVoltage = 1;
+    private static final double ejectVoltage = 1; // TODO: tune
+    private static final double intakeVoltage = 1; // TODO: tune
 
 
     public CoralMech() {
-        // motor configuration
+        // Create motor configuration to apply to the wheel motors
         SparkMaxConfig config = new SparkMaxConfig();
         // kCoast might be worth trying but these wheels will be powering the motors holding the coral in place
         config.idleMode(IdleMode.kBrake);
-        motorL.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-        motorR.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_motorL.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
+        m_motorR.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
         SmartDashboard.putNumber("CoralMech/ejectVoltage", ejectVoltage);
     }
 
+    /**
+     * Runs the coral wheel motors at ejectVoltage in opposite directions
+     */
     public void placeCoral() {
-        motorL.setVoltage(ejectVoltage);
-        motorR.setVoltage(-ejectVoltage);
+        m_motorL.setVoltage(ejectVoltage);
+        m_motorR.setVoltage(-ejectVoltage);
         SmartDashboard.putString("CoralMech/State", "placing");
     }
 
+    /**
+     * Runs the coral wheels at intakeVoltage in opposite directions
+     */
     public void intakeCoral() {
-        motorL.setVoltage(intakeVoltage);
-        motorR.setVoltage(-intakeVoltage);
+        m_motorL.setVoltage(intakeVoltage);
+        m_motorR.setVoltage(-intakeVoltage);
         SmartDashboard.putString("CoralsMech/State", "intaking");
     }
 
