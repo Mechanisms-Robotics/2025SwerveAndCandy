4d7a7df267767f6477567fe4b49c88a6815c892a
Joel Odom <joelodom@gmail.com>
2025-02-27 17:50:18 -0500

    Revert "Merge branch 'pre-gainesville' into hold-elevator-set"
    This reverts commit 318896513fb41771dc93215d447e5cbe7a04ae92, reversing
    changes made to de6b2d4a8bf613c98ed45f52a965ba1eec60c720.

--- src/main/java/frc/robot/commands/L2.java
+++ src/main/java/frc/robot/commands/L2.java
@@ -12,15 +12,6 @@ public class L2 extends Command {
     private final AlgaeMech m_algaeMech;
     private final Supplier<Boolean> clutch;
 
-    /**
-     * Raise the elevator to the L2 position.
-     * If the clutch is engaged, the elevator will raise a little heigher and the algae arms
-     * will angle down to grab the algae.
-     * 
-     * @param elevator used for raising the elevator to L2
-     * @param algaeMech used for angleing the algae mechanism when grabbing algae
-     * @param clutch button boolean supplier for determining if it is in algae mode
-     */
     public L2(Elevator elevator, AlgaeMech algaeMech, Supplier<Boolean> clutch) {
         m_elevator = elevator;
         m_algaeMech = algaeMech;
