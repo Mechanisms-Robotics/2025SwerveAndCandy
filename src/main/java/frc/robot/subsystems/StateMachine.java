e7883dcfdd9c36cdff1cc6517185546e03e6de6d
Nolan Martzall <Nolan.M@themechanisms.org>
2025-03-22 10:29:12 -0400

    Made state machine

--- src/main/java/frc/robot/subsystems/StateMachine.java
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -1,22 +1,26 @@
 package frc.robot.subsystems;
 
 public class StateMachine {
-    
-}
 
-public enum State {
-    /**No AprilTag seen by the robot */
-    NoTargetIdentified,
-    /**AprilTag seen on the reef */
-    DetectedReefTarget,
-    /**Going towards AprilTag on the reef */
-    DrivingToReefTarget,
-    /**Arrived at AprilTag on the reef */
-    ArrivedAtReefTarget
-}
+    public enum State {
+        /**No AprilTag seen by the robot */
+        NoTargetIdentified,
+        /**AprilTag seen on the reef */
+        DetectedReefTarget,
+        /**Going towards AprilTag on the reef */
+        DrivingToReefTarget,
+        /**Arrived at AprilTag on the reef */
+        ArrivedAtReefTarget
+    }
 
-private State state = State.NoTargetIdentified;
+    private State myState = State.NoTargetIdentified;
 
-public State getState() {
-    return state;
+    public State getState() {
+        return myState;
+    }
+
+    public void setState(State newState) {
+        myState = newState;
+        return;
+    }
 }
