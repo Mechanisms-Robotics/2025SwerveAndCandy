73ddedff6341b1db55fe4cfbd0ace0b7e034733a
Nolan Martzall <Nolan.M@themechanisms.org>
2025-03-22 09:50:46 -0400

    Start of state machine

--- src/main/java/frc/robot/subsystems/StateMachine.java
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems;
 
 public class StateMachine {
-// EEEEEEEEE
+    
+}
+
+public enum State {
+    /**No AprilTag seen by the robot */
+    NoTargetIdentified,
+    /**AprilTag seen on the reef */
+    DetectedReefTarget,
+    /**Going towards AprilTag on the reef */
+    DrivingToReefTarget,
+    /**Arrived at AprilTag on the reef */
+    ArrivedAtReefTarget
+}
+
+private State state = State.NoTargetIdentified;
+
+public State getState() {
+    return state;
 }
