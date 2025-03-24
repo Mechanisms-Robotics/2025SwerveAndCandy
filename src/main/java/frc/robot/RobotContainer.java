5225c2795d4485b09579f923b3c326932abf7931
MicahMaphet <micahmaphet@gmail.com>
2025-03-24 09:40:11 -0700

    fix refactor for CoralStationLineup

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,8 @@ import frc.robot.commands.L3;
 import frc.robot.commands.autos.TimedLeave;
 import frc.robot.commands.swervedrive.auto.AutoReefLineup;
 import frc.robot.commands.swervedrive.auto.BargeAlign;
-import frc.robot.commands.swervedrive.auto.ProcessorLineup;
+import frc.robot.commands.swervedrive.auto.CoralStationLineup;
+import frc.robot.commands.swervedrive.auto.CoralStationLineup;
 import frc.robot.subsystems.AlgaeMech;
 import frc.robot.subsystems.CoralMech;
 import frc.robot.subsystems.Elevator;
@@ -174,7 +175,7 @@ public class RobotContainer {
 
       xboxController.a().whileTrue(new AutoReefLineup(m_drivebase, xboxController.rightBumper()));
       xboxController.b().whileTrue(new BargeAlign(m_drivebase, () -> -xboxController.getLeftX()));
-      xboxController.x().whileTrue(new ProcessorLineup(m_drivebase));
+      xboxController.x().whileTrue(new CoralStationLineup(m_drivebase));
       m_drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocityXbox);
     } else
     {
