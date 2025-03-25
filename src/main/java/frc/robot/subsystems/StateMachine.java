a0e4bc31fb2b042c09a43d916ec069c20002255b
Michael Fox <foxms1@gmail.com>
2025-03-25 18:49:39 -0400

    updated constructors, setup placeholders for adding light controls

---
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -0,0 +1,90 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class StateMachine extends SubsystemBase{
+
+    public enum State {
+        /**No AprilTag seen by the robot */
+        NoTargetIdentified,
+        /**AprilTag seen on the reef */
+        DetectedReefTarget,
+        /**Going towards AprilTag on the reef */
+        DrivingToReefTarget,
+        /**Arrived at AprilTag on the reef */
+        ArrivedAtReefTarget
+    }
+
+    private State myState;
+
+    /** Constructor that takes an initial state as a parameter */
+    public StateMachine(State initialState) {
+        /* call method to set initial state and configure lights */
+        setState(initialState);
+    }
+
+    /** default constructor with no parameter will
+     * set initial state to "NoTargetIdentified"
+     */
+    public StateMachine() {
+        /* Call other constructur and pass default value
+         */
+        this(State.NoTargetIdentified);
+    }
+
+    public State getState() {
+        return myState;
+    }
+
+    public void setState(State newState) {
+        switch (newState) {
+            case NoTargetIdentified:
+                setNotIdentified();
+                break;
+            case DetectedReefTarget:
+                setDetectedReefTarget();
+                break;
+            case DrivingToReefTarget:
+                setDrivingToReefTarget();
+                break;
+            case ArrivedAtReefTarget:
+                setArrivedAtReefTarget();
+                break;
+            default:
+                break;
+        }
+        return;
+    }
+
+    private void setNotIdentified() {
+        myState = State.NoTargetIdentified;
+        
+        // TODO: turn on white light
+
+        return;
+    }
+
+    private void setDetectedReefTarget() {
+        myState = State.DetectedReefTarget;
+
+        // TODO: turn on green light
+
+        return;
+    }
+
+    private void setDrivingToReefTarget() {
+        myState = State.DrivingToReefTarget;
+
+        // TODO: turn on blue light
+
+        return;
+    }
+
+    private void setArrivedAtReefTarget() {
+        myState = State.ArrivedAtReefTarget;
+
+        // TODO: turn on red light
+        
+        return;
+    }
+}
