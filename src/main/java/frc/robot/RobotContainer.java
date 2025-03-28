50819cc8624ded558db649f4bec29944bf6398e3
Nolan Martzall <Nolan.M@themechanisms.org>
2025-03-27 21:00:50 -0400

    Made a new auto (for the robot's right), and made changes (attempted bug fixes) to existing auto (for robot's left)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -321,7 +321,8 @@ public class RobotContainer {
     m_autoChooser.addOption("L2 Coral", new PathPlannerAuto("L2 Coral")); 
     m_autoChooser.addOption("L2 Coral Reflected", new PathPlannerAuto("L2 Coral Reflected")); 
     m_autoChooser.addOption("Test Auto", new PathPlannerAuto("Test Auto"));
-    m_autoChooser.addOption("two coral", MyAutoBuilder.twoCoral(m_drivebase, m_elevator, m_algaeMech, m_coralMech));
+    m_autoChooser.addOption("Two Coral Left", MyAutoBuilder.twoCoralLeft(m_drivebase, m_elevator, m_algaeMech, m_coralMech));
+    m_autoChooser.addOption("Two Coral Right", MyAutoBuilder.twoCoralRight(m_drivebase, m_elevator, m_algaeMech, m_coralMech));
 
     SmartDashboard.putData("Auto Choose", m_autoChooser);
   }
--- src/main/java/frc/robot/commands/autos/MyAutoBuilder.java
+++ src/main/java/frc/robot/commands/autos/MyAutoBuilder.java
@@ -17,12 +17,12 @@ import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 public class MyAutoBuilder {
-    public static Command twoCoral(SwerveSubsystem swerve, Elevator elevator, AlgaeMech algaeMech, CoralMech coralMech) {
+    public static Command twoCoralLeft(SwerveSubsystem swerve, Elevator elevator, AlgaeMech algaeMech, CoralMech coralMech) {
         Pose2d reefTarget1 = DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)
-        ? FieldConstants.BLUE_REEF_POSES.get(20).getSecond()
+        ? FieldConstants.BLUE_REEF_POSES.get(22).getSecond()
         : FieldConstants.RED_REEF_POSES.get(9).getSecond();
         Pose2d reefTarget2 = DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)
-        ? FieldConstants.BLUE_REEF_POSES.get(19).getSecond()
+        ? FieldConstants.BLUE_REEF_POSES.get(17).getSecond()
         : FieldConstants.RED_REEF_POSES.get(8).getSecond();
         Pose2d coralStationTarget = DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)
         ? FieldConstants.BLUE_CORAL_STATION_RIGHT
@@ -31,6 +31,69 @@ public class MyAutoBuilder {
         String ntTable = "Commands/auto/2 L4 coral/";
 
         return Commands.sequence(
+            Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state", "I am driving to the reef (" + reefTarget1.toString() + ")"))
+            , new PIDtoPosition(swerve, reefTarget1)
+                .until(() -> swerve.isNear(reefTarget1))
+                    .withTimeout(5)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+                "I am raising the elevator to L4. I got " + swerve.getMyPose().getTranslation().getDistance(reefTarget1.getTranslation())
+                + " meters close to my target"))
+            , new L4(elevator)
+                .until(elevator::atGoal)
+                    .withTimeout(3)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+              "I am feeding coral, waiting to stop"))
+            , Commands.runOnce(coralMech::feed)
+            , new WaitCommand(1)
+            , Commands.runOnce(coralMech::stop)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+              "I stopped feeding coral and am now lowering the elevator to rest"))
+            , new ElevatorRest(elevator, algaeMech)
+                .until(() -> Math.abs(elevator.getCurrentPosition() - Elevator.RESTING) < 5000)
+                    .withTimeout(5)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+              " I am now moving to the coral station"))
+            , new PIDtoPosition(swerve, coralStationTarget)
+                .until(() -> swerve.isNear(coralStationTarget))
+                    .withTimeout(5)
+            , new WaitCommand(2)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+              " I am now moving to my second reef target"))
+            , new PIDtoPosition(swerve, reefTarget2)
+                .until(() -> swerve.isNear(reefTarget2))
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+              "I am now moving elevator to L4"))
+            , new L4(elevator)
+                .until(elevator::atGoal)
+                    .withTimeout(2)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state",
+              "I am now feeding coral"))
+            , Commands.runOnce(coralMech::feed)
+            , new WaitCommand(2)
+            , Commands.runOnce(coralMech::stop)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state", "I am lowering the elevator to rest"))
+            , new ElevatorRest(elevator, algaeMech)
+                .until(elevator::atGoal)
+                    .withTimeout(5)
+            , Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state", "I am driving to the coral station again"))
+            , new PIDtoPosition(swerve, coralStationTarget)
+        );
+    }
+
+    public static Command twoCoralRight(SwerveSubsystem swerve, Elevator elevator, AlgaeMech algaeMech, CoralMech coralMech) {
+      Pose2d reefTarget1 = DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)
+      ? FieldConstants.BLUE_REEF_POSES.get(19).getSecond()
+      : FieldConstants.RED_REEF_POSES.get(6).getSecond();
+      Pose2d reefTarget2 = DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)
+      ? FieldConstants.BLUE_REEF_POSES.get(20).getSecond()
+      : FieldConstants.RED_REEF_POSES.get(11).getSecond();
+      Pose2d coralStationTarget = DriverStation.getAlliance().equals(DriverStation.Alliance.Blue)
+      ? FieldConstants.BLUE_CORAL_STATION_LEFT
+      : FieldConstants.RED_CORAL_STATION_LEFT;
+
+      String ntTable = "Commands/auto/2 L4 coral/";
+
+      return Commands.sequence(
             Commands.runOnce(() -> SmartDashboard.putString(ntTable + "state", "I am driving to the reef (" + reefTarget1.toString() + ")"))
             , new PIDtoPosition(swerve, reefTarget1)
                 .until(() -> swerve.isNear(reefTarget1))
